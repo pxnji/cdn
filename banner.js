553a2f99295f5a650e6e1d9bc7813ab77a32210c
pxnji <panjilesm@gmail.com>
2019-04-06 00:07:04 +0800

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,1 @@
+void 0!==app.session.user&&(document.getElementById("welcomeButton").style.display="none"),void 0!==app.session.user&&(document.getElementById("welcomeButton2").style.display="none");
